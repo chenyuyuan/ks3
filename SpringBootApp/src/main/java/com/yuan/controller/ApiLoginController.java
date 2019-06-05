57b3624298e3a99a2de5eff7a62379885534d80d
chenyuyuan <1695215206@qq.com>
2019-06-06 00:24:47 +0800

    home page

--- SpringBootApp/src/main/java/com/yuan/controller/ApiLoginController.java
+++ SpringBootApp/src/main/java/com/yuan/controller/ApiLoginController.java
@@ -32,18 +32,17 @@ public class ApiLoginController {
         String account = params.getString("account");
         String password = params.getString("password");
 
-        Hashtable hashtable=new Hashtable();//存放要返回的数据
-        HttpSession session=request.getSession();//获取request请求里的session, 如果是第一次请求, 则会创建一个新的session
+        Hashtable hashtable = new Hashtable();//存放要返回的数据
+        HttpSession session = request.getSession();//获取request请求里的session, 如果是第一次请求, 则会创建一个新的session
 
         System.out.println("正在登陆");
-        System.out.println("账号:"+account+" 密码:"+password);
+        System.out.println("账号:" + account + " 密码:" + password);
 
-        if(account==null||password==null){
+        if(account == null || password == null){
             String message = String.format("wronginput");
             return ResultFactory.buildFailResult(message);
         }
-        String getpass=usersService.selectUserByAccountGetPasswprd(account);
-        //System.out.println("account:"+account+" password:"+password);
+        String getpass = usersService.selectUserByAccountGetPasswprd(account);
         if (!Objects.equals(getpass, password)) {
             String message = String.format("loginerror");
             return ResultFactory.buildFailResult(message);
@@ -63,34 +62,26 @@ public class ApiLoginController {
     @RequestMapping(value = "/register", method = RequestMethod.POST, produces = "application/json; charset=UTF-8")
     @ResponseBody
     public Result Register(HttpServletRequest request, HttpServletResponse response) {
-        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, accept, content-type, xxxx, application/json");
-        response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, PATCH");
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-        response.setHeader("Access-Control-Allow-Credentials", String.valueOf(true));
+        String account = request.getParameter("account");
+        String password = request.getParameter("password");
+        String telephone = request.getParameter("telephone");
+        Hashtable hashtable = new Hashtable();//存放要返回的数据
+        System.out.print("account:" + account + ",password:" + password + ",telephone:" + telephone);
 
-        String account=request.getParameter("account");
-        String password=request.getParameter("password");
-        String telephone=request.getParameter("telephone");
-        Hashtable hashtable=new Hashtable();//存放要返回的数据
-        System.out.print("account:"+account+",password:"+password+",telephone:"+telephone);
-
-        if(account==null||password==null){
+        if(account == null || password == null) {
             String message = String.format("注册失败，详细信息[用户名或密码为空]。");
             return ResultFactory.buildFailResult(message);
         }
 
         String foundPwd=usersService.selectUserByAccountGetPasswprd(account);
-        //System.out.println("account:"+account+" password:"+password);
         if (foundPwd != null || foundPwd == "") {
             String message = String.format("注册失败，详细信息[用户名已存在]。");
             return ResultFactory.buildFailResult(message);
         }
-        else{
-            usersService.toRegister(account,password,telephone);
+        else {
+            usersService.toRegister(account, password, telephone);
             String message = String.format("注册成功。");
             return ResultFactory.buildSuccessResult(message);
         }
-
-
     }
 }
