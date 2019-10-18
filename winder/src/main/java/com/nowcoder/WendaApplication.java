4538d8631e558d593af274303a2baeec357f4c11
chenyuyuan <1695215206@qq.com>
2019-10-18 16:30:50 +0800

    save

--- winder/src/main/java/com/nowcoder/WendaApplication.java
+++ winder/src/main/java/com/nowcoder/WendaApplication.java
@@ -2,6 +2,7 @@ package com.nowcoder;
 
 import com.nowcoder.model.User;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
